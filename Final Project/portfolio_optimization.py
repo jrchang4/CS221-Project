c20ff415c55796bd3c66f2e627a7ebb74323d160
Joshua <Chang>
2019-05-24 14:26:21 -0700

    starting to implement mdp, qlearning

--- Final Project/portfolio_optimization.py
+++ Final Project/portfolio_optimization.py
@@ -1,16 +1,14 @@
 #import data
-
-
-# A useful library for reading files
-# with "comma seperated values".
 import csv
+import util, math, random, collections
+
 
 # The main method
 def main():
 	data = loadCsvData('all_stocks_5yr.csv')
 	data = sortData(data)
 	print len(data)
-	printData(data["AAL"])
+	printData(data["T"])
 
 # Reads a files into a 2d array. There are
 # other ways of doing this (do check out)
@@ -51,6 +49,44 @@ def sortData(matrix):
 
 	return sorted_data
 
+
+class QLearningAlgorithm:
+	def __init__(self, actions, discount, featureExtractor, explorationProb=0.2):
+        self.actions = actions
+        self.discount = discount
+        self.featureExtractor = featureExtractor
+        self.explorationProb = explorationProb
+        self.weights = defaultdict(float)
+        self.numIters = 0
+
+   	def getQ(self, state, action):
+   		score = 0
+   		for f, v in self.featureExtractor(state, action):
+   			score += self.weights[f]*v
+   		return score
+
+   	def getAction(self, state):
+   		self.numIters += 1
+   		return max((self.getQ(state, action), action) for action in self.actions(state))[1]
+
+   	def getStepsize(self):
+   		return 1.0/math.sqrt(self.numIters)
+
+   	def incorporateFeedback(self, state, action, reqard, newState):
+   		
+   		Vopt = 0
+        if newState != None:
+            Vopt = max((self.getQ(newState, a), a) for a in self.actions(newState))[0]
+
+        prediction = self.getQ(state,action) #float
+        target = reward + self.discount*Vopt #float
+        phi = self.featureExtractor(state, action) #dictionary
+        
+        for f, v in self.featureExtractor(state, action):
+            self.weights[f] = self.weights[f] - self.getStepSize()*(prediction - target)*v 
+
+def featureExtractor(state, action):
+	
 
 
 # This if statement passes if this
---
+++ Final Project/util.py
@@ -0,0 +1,7 @@
+import collections, random
+
+
+class PortfolioMdp:
+	def__init__(self, data): self.data = data
+
+	def startState
