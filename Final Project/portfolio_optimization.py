a010a75ac4d0e391339337285ec7bf5f6cda1665
Joshua <Chang>
2019-06-01 23:36:54 -0700

    Completed portfolioMDP Class

--- Final Project/portfolio_optimization.py
+++ Final Project/portfolio_optimization.py
@@ -6,13 +6,106 @@ import numpy as np
 
 # The main method
 def main():
-	stock_dict = loadCsvData('all_stocks_5yr.csv')
-	stock_dict = sortData(stock_dict)
+	data = loadCsvData('all_stocks_5yr.csv')
+	data = data[1:]
+	stock_dict = sortData(data)
+	prev_stock_dict = sortPrevData(data)
+	#print prev_stock_dict
 	#print len(stock_dict)
-	#printData(stock_dict["T"])
-	mdp = util.PortfolioMdp(stock_dict, 7)
-	stocks_t = mdp.stocks(0)
-	print len(stocks_t)
+	#print len(stock_dict["T"])
+	mdp = util.PortfolioMdp(stock_dict, 7, 100)
+	#print stock_dict['FIS'][251][4]
+	#print mdp.startState()
+	
+	random_selection = []
+	for _ in range(20):
+		random_selection.append(generateRandom(stock_dict, 10))
+
+	print np.mean(random_selection)
+	print generateLowRisk(stock_dict,10)
+	
+	
+	#print generateRandom(stock_dict, 10)
+
+def generateRandom(stock_dict, num_stocks):
+	companies = stock_dict.keys()
+	orig_investment = 100
+	total_return = orig_investment
+	for date in range(252):
+		indices = random.sample(range(len(stock_dict)), num_stocks)
+		daily_return = 0
+		for i in indices: #get the return for each stock on a given day
+			company = companies[i]
+			daily_return += calculateReward(stock_dict, company, total_return, num_stocks, date)
+		#print daily_return
+		total_return += daily_return
+	return float(total_return)/orig_investment
+
+		
+def generateLowRisk(stock_dict, num_stocks):
+	companies = stock_dict.keys()
+	orig_investment = 100
+	total_return = orig_investment
+	for date in range(252):
+		indices = findLowRisk(stock_dict)
+		daily_return = 0
+		for i in indices: #get the return for each stock on a given day
+			company = companies[i]
+			daily_return += calculateReward(stock_dict, company, total_return, num_stocks, date)
+		#print daily_return
+		total_return += daily_return
+	return float(total_return)/orig_investment
+
+def findLowRisk(prev_stock_dict):
+	company_stds = []
+	for company in prev_stock_dict:
+		deltas = []
+		for date in range(len(prev_stock_dict[company])):
+			open_price = prev_stock_dict[company][date][1]
+			close_price = prev_stock_dict[company][date][4]
+			if open_price == "" or close_price == "":
+				open_price = prev_stock_dict[company][date -1][1]
+				close_price = prev_stock_dict[company][date-1][4]
+			open_price = float(open_price)	
+			close_price = float(close_price)
+			delta = close_price - open_price 
+			deltas.append(abs(delta))
+		std = np.std(deltas)
+		company_stds.append(std)
+	indices = []
+	for _ in range(10):
+		i = company_stds.index(min(company_stds))
+		indices.append(i)
+		company_stds.pop(i)
+
+	return indices
+
+
+
+	
+
+
+
+
+
+
+
+def calculateReward(stock_dict, company, reward, num_stocks, date):
+	open_price = stock_dict[company][date][1]
+	close_price = stock_dict[company][date][4]
+	open_price = float(open_price)	
+	close_price = float(close_price)
+	delta = close_price - open_price 
+	investment = float(reward)/num_stocks
+	num_shares = float(investment)/open_price
+
+	stock_return = num_shares * delta
+	#print stock_return
+	return stock_return
+
+
+
+	
 
 
 # Reads a files into a 2d array. There are
@@ -44,63 +137,88 @@ def printData(matrix):
 def sortData(matrix):
 	sorted_data = {}
 	previous_company = "default"
+	matrix = np.array(matrix)
 	for row in matrix:
 		company = row[6]
-		if company != previous_company and row[0] == '2013-02-08':
+		if row[0][:4] == '2015':
+			if company != previous_company:
+				sorted_data[company] = [[row[i] for i in range(6)]]
+				previous_company = company
+			else:
+				if company in sorted_data:
+						sorted_data[company].append([row[i] for i in range(6)])
+
+	sorted_data_trimmed = {}
+	for data in sorted_data:
+		if len(sorted_data[data]) == 252:
+			sorted_data_trimmed[data] = sorted_data[data]
+	
+
+	return sorted_data_trimmed
+
+
+def sortPrevData(matrix):
+	sorted_data = {}
+	previous_company = "default"
+	for row in matrix:
+		company = row[6]
+		if row[0][:4] == '2015':
+			break
+		if company != previous_company:
 			sorted_data[company] = [[row[i] for i in range(6)]]
 			previous_company = company
 		else:
 			if company in sorted_data:
 					sorted_data[company].append([row[i] for i in range(6)])
+	
 
 	return sorted_data
 
 
 class QLearningAlgorithm:
-    def __init__(self, actions, discount, featureExtractor, explorationProb=0.2):
-        self.actions = actions
-        self.discount = discount
-        self.featureExtractor = featureExtractor
-        self.explorationProb = explorationProb
-        self.weights = defaultdict(float)
-        self.numIters = 0
+	def __init__(self, actions, discount, featureExtractor):
+		self.actions = actions
+		self.discount = discount
+		self.featureExtractor = featureExtractor
+		self.weights = defaultdict(float)
+		self.numIters = 0
 
-    # Return the Q function associated with the weights and features
-    def getQ(self, state, action):
-        score = 0
-        for f, v in self.featureExtractor(state, action):
-            score += self.weights[f] * v
-        return score
+	# Return the Q function associated with the weights and features
+	def getQ(self, state, action):
+		score = 0
+		for f, v in self.featureExtractor(state, action):
+			score += self.weights[f] * v
+		return score
 
-    # This algorithm will produce an action given a state.
-    # Here we use the epsilon-greedy algorithm: with probability
-    # |explorationProb|, take a random action.
-    def getAction(self, state):
-        self.numIters += 1
-        if random.random() < self.explorationProb:
-            return random.choice(self.actions(state))
-        else:
-            return max((self.getQ(state, action), action) for action in self.actions(state))[1]
+	# This algorithm will produce an action given a state.
+	# Here we use the epsilon-greedy algorithm: with probability
+	# |explorationProb|, take a random action.
+	def getAction(self, state):
+		self.numIters += 1
+		if random.random() < self.explorationProb:
+			return random.choice(self.actions(state))
+		else:
+			return max((self.getQ(state, action), action) for action in self.actions(state))[1]
 
-    # Call this function to get the step size to update the weights.
-    def getStepSize(self):
-        return 1.0 / math.sqrt(self.numIters)
+	# Call this function to get the step size to update the weights.
+	def getStepSize(self):
+		return 1.0 / math.sqrt(self.numIters)
 
-    # We will call this function with (s, a, r, s'), which you should use to update |weights|.
-    # Note that if s is a terminal state, then s' will be None.  Remember to check for this.
-    # You should update the weights using self.getStepSize(); use
-    # self.getQ() to compute the current estimate of the parameters.
-    def incorporateFeedback(self, state, action, reward, newState):
-        # BEGIN_YOUR_CODE (our solution is 12 lines of code, but don't worry if you deviate from this)
-        change = 0
-        if newState != None:
-            change = self.getStepSize() * (self.getQ(state, action) - (reward + self.discount * max(self.getQ(newState, action) for action in self.actions(newState))))
-        for feature in self.featureExtractor(state, action):
-            featureName = feature[0]
-            featureValue = feature[1]
-            change = change * featureValue
-            self.weights[featureName] -= change
-        # END_YOUR_CODE
+	# We will call this function with (s, a, r, s'), which you should use to update |weights|.
+	# Note that if s is a terminal state, then s' will be None.  Remember to check for this.
+	# You should update the weights using self.getStepSize(); use
+	# self.getQ() to compute the current estimate of the parameters.
+	def incorporateFeedback(self, state, action, reward, newState):
+		# BEGIN_YOUR_CODE (our solution is 12 lines of code, but don't worry if you deviate from this)
+		change = 0
+		if newState != None:
+			change = self.getStepSize() * (self.getQ(state, action) - (reward + self.discount * max(self.getQ(newState, action) for action in self.actions(newState))))
+		for feature in self.featureExtractor(state, action):
+			featureName = feature[0]
+			featureValue = feature[1]
+			change = change * featureValue
+			self.weights[featureName] -= change
+		# END_YOUR_CODE
 
 
 #def featureExtractor(state, action):
--- Final Project/util.py
+++ Final Project/util.py
@@ -1,12 +1,24 @@
 import collections, random
+import numpy as np
 
 
 class PortfolioMdp:
-	def __init__(self, stock_dict, N): 
+	def __init__(self, stock_dict, N, investment): 
 		 	self.stock_dict = stock_dict
-			self.N  = N
-			self.allocation = []
+			self.N  = N #lookback window
+			self.allocation = initialize_allocation()
+			self.investment = investment
+			self.endTime = 100
+			self.capital = 10000
 
+	def initialize_allocation():
+		allocation = {}
+		for stock in self.stock_dict:
+			allocation[stock] = 0
+		return allocation
+
+
+	#Returns the stock data for a given day, t
 	def stocks(self, t):
 		stocks = []
 		for stock in self.stock_dict:
@@ -14,5 +26,88 @@ class PortfolioMdp:
 		return stocks
 
 	def startState(self):
-		return 
+		state = []
+		state.append([self.stocks(t) for t in range(self.N)]) #stock data for previous N days
+		state.append(self.allocation) #dictionary
+		state.append(self.N) #start time at N
+		state.append(self.capital)
+
+		return state
+
+	def actions(self, state):
+		return ['Buy', 'Sell', 'Stay']
+
+	def calculateStockChanges(self, stock):
+		open_price = stock[1]
+		close_price = stock[4]
+
+		percent_change = (close_price - open_price)/float(open_price)
+		
+		return percent_change
+
+
+	def calcReward(self, remaining_capital, allocation):
+		total_invested = sum(allocation.values())
+		return total_invested + remaining_capital
+
+
+
+	#state = [[stock data], {allocation}, current time, capital]
+	def succStates(self, state, action):
+
+		if state[2] == self.endTime:
+			return []
+
+		results = []
+
+		stocks = state[0][-1]
+		new_time = state[2] + 1
+		new_data = [self.stocks(t) for t in range(new_time - self.N, new_time)]
+		curr_capital = state[3]
+		allocation = state[1]
+
+		if action == 'Buy':
+			if state[3] > 1000:
+				for stock in stocks:
+					allocation[stock] += curr_capital * 0.1
+					remaining_capital = 0.9 * curr_capital 
+					allocation[stock] *= calculateStockChanges(stock)
+					newState = [new_data, allocation, new_time, remaining_capital]
+					reward = calcReward(remaining_capital, allocation)
+					results.append(newState, reward)
+
+		elif action == 'Sell':
+			for stock in stocks:
+				change = allocation[stock] * 0.1
+				allocation[stock] -= change
+				allocation[stock] *= calculateStockChanges(stock)
+				remaining_capital = curr_capital + change
+				newState = [new_data, allocation, new_time, remaining_capital]
+				reward = calcReward(remaining_capital, allocation)
+				results.append(newState, reward)
+ 
+		else: # Do nothing
+			for stock in stocks
+				allocation[stock] *= calculateStockChanges(stock)
+			newState = [new_data, allocation, new_time, curr_capital]
+			results.append(newState)
+
+		return results
+
+
+	def discount(self, discount):
+		return discount
+
+    def computeStates(self):
+	    self.states = set()
+	    queue = []
+	    self.states.add(self.startState())
+	    queue.append(self.startState())
+	    while len(queue) > 0:
+	        state = queue.pop()
+	        for action in self.actions(state):
+	            for newState, prob, reward in self.succStates(state, action):
+	                if newState not in self.states:
+	                    self.states.add(newState)
+	                    queue.append(newState)
 
